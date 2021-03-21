0b3c63d234b57bc5165aa63b5ce5195ce113b5b4
Your Name <you@example.com>
2021-03-20 22:42:39 -0400

    setting time counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,7 @@ gameQuiz.prototype.guess = function(answer) {
     if(this.getQuestionIndex().isCorrectAnswer(answer)) {
         this.score++;
     }
+    else seconds-=10;
  
     this.questionIndex++;
 }
@@ -32,7 +33,7 @@ Question.prototype.isCorrectAnswer = function(choice) {
 }
  
  
-function populate() {
+function startGame() {
     if(quiz.isEnded()) {
         showScores();
     }
@@ -57,7 +58,7 @@ function guess(id, guess) {
     var button = document.getElementById(id);
     button.onclick = function() {
         quiz.guess(guess);
-        populate();
+        startGame();
     }
 };
  
@@ -67,12 +68,28 @@ function showScores() {
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
 };
+
+// setting counter
+var seconds = 100, $seconds = document.querySelector('#countdown');
+(function countdown() {
+    $seconds.textContent = seconds + ' second' + (seconds == 1 ?  '' :  's')
+    if(seconds --> 0) setTimeout(countdown, 1000)
+})();
  
 // create questions here
 var questions= [
-    new Question("Prime Minister of Canada?",["Justin Trudeau","Doug Ford","Jonh Tori","Mother Teresa"],"Justin Trudeau"),
-    new Question("President of USA?",["Donald Trump","Barak Obama","Joe Biden","George Bush"],"Joe Biden"),
-    new Question("2+2?",["4","0","-4","none"],"4")
+    new Question("10x0?",["0","9","4","6"],"0"),
+    new Question("4-1?",["12","4","3","7"],"3"),
+    new Question("Area of Circle?",["πrg","πr2","πr2h","none"],"πr2"),
+    new Question("10x10?",["34","9","84","100"],"100"),
+    new Question("47-1?",["12","47","30","46"],"46"),
+    new Question("23+2?",["4","0","-4","25"],"25"),
+    new Question("12/3?",["0","9","4","6"],"4"),
+    new Question("42/1?",["12","42","38","17"],"42"),
+    new Question("2+25?",["4","0","-4","none"],"none"),
+    new Question("10/0?",["0","undefined","4","6"],"undefined"),
+    new Question("4+1?",["5","4","3","7"],"5"),
+    new Question("π?",["3.14","0","-4","none"],"3.14")
 ];
 
  
@@ -80,4 +97,4 @@ var questions= [
 var quiz = new gameQuiz(questions);
  
 // display quiz
-populate();
+startGame();
